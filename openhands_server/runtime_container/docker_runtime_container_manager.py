0394d798a99dca384f0fd06546513f1879d08686
Tim O'Farrell <tofarr@gmail.com>
2025-09-12 15:19:33 -0600

    WIP

--- openhands_server/runtime_container/docker_runtime_container_manager.py
+++ openhands_server/runtime_container/docker_runtime_container_manager.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 import docker
 
 from openhands_server.runtime_container.runtime_container_manager import RuntimeContainerManager
+from openhands_server.runtime_image.docker_runtime_image_manager import DockerRuntimeImageManager
 
 
 
@@ -11,4 +12,4 @@ class DockerRuntimeContainerManager(RuntimeContainerManager):
 
     client: docker.DockerClient = field(default_factory=docker.from_env)
     container_name_prefix: str = "openhands-runtime-"
-
+    runtime_image_manager: DockerRuntimeImageManager = field(default_factory=DockerRuntimeImageManager.get_instance)
