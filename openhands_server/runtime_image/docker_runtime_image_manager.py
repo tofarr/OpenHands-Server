6404c06f9ce2948fb088245b18734e0c976317b3
Tim O'Farrell <tofarr@gmail.com>
2025-09-12 14:39:38 -0600

    DockerRuntimeImageManager implementation

--- openhands_server/runtime_image/docker_runtime_image_manager.py
+++ openhands_server/runtime_image/docker_runtime_image_manager.py
@@ -1,19 +1,19 @@
 
-import asyncio
 from dataclasses import dataclass, field
-from datetime import datetime
-from uuid import UUID
 import docker
-from docker.errors import NotFound, APIError
 from openhands_server.runtime_image.runtime_image_manager import RuntimeImageManager
-from openhands_server.runtime_image.model import RuntimeImageInfo, RuntimeImageInfoPage, RuntimeImageStatus
 
 
 @dataclass
 class DockerRuntimeImageManager(RuntimeImageManager):
+    """Runtime manager for docker images. By default, all images with the name given are loaded and returned (They may have different ta)"""
 
     client: docker.DockerClient = field(default_factory=docker.from_env)
-    image_name_prefix: str = "ohrt-",
+    repository: str = "ghcr.io/all-hands-ai/runtime"
+    command: str = "python -u -m openhands_server.runtime"
+    initial_env: dict[str, str] = field(default_factory=dict)
+    exposed_ports: dict[int, str] = field(default_factory=dict, description="Exposed ports to be mapped to endpoint urls in the resulting container")
+    working_dir: str = '/openhands/code'
 
     async def search_runtime_image_info(image_name__eq = None, page_id = None, limit = 100):
         raise NotImplementedError
--- openhands_server/runtime_image/model.py
+++ openhands_server/runtime_image/model.py
@@ -14,13 +14,19 @@ class RuntimeImageStatus(Enum):
     DELETING = "DELETING"
 
 
+class ExposedPort(BaseModel):
+    port: int
+    description: str | None = None
+
+
 class RuntimeImageInfo(BaseModel):
     """ A runtime image is a template for creating a runtime, analogous to a docker image """
     id: str
     image_name: str
     command: str
     created_at: datetime
-    initial_env: dict[str, str] = Field(description="Initial Environment Variables")
+    initial_env: dict[str, str] = Field(default_factory=dict, description="Initial Environment Variables")
+    exposed_ports: dict[int, str] = Field(default_factory=dict, description="Exposed ports to be mapped to endpoint urls in the resulting container")
     working_dir: str = '/openhands/code'
 
 
