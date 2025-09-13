d51226371184b95636db4257b7a4aafaa3ea37d6
Tim O'Farrell <tofarr@gmail.com>
2025-09-13 08:34:11 -0600

    WIP

--- openhands_server/runtime_image/runtime_image_router.py
+++ openhands_server/runtime_image/runtime_image_router.py
@@ -3,15 +3,12 @@
 from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from openhands import get_user_id
-
-from openhands_server.runtime_image.model import RuntimeImageInfo, RuntimeImageInfoPage
-from openhands_server.runtime_image.runtime_image_service import RuntimeImageManager, get_default_runtime_image_manager
-from openhands_server.utils.success import Success
+from openhands_server.runtime_image.runtime_image_models import RuntimeImageInfo, RuntimeImageInfoPage
+from openhands_server.runtime_image.runtime_image_service import RuntimeImageService, get_default_runtime_image_service
 
 router = APIRouter(prefix="/runtime-images")
-runtime_image_manager: RuntimeImageManager = get_default_runtime_image_manager()
-router.lifespan(runtime_image_manager)
+runtime_image_service: RuntimeImageService = get_default_runtime_image_service()
+router.lifespan(runtime_image_service)
 
 # Read methods
 
@@ -19,12 +16,12 @@ router.lifespan(runtime_image_manager)
 async def search_runtime_images(page_id: str | None = None, limit: int = 100) -> RuntimeImageInfoPage:
     assert limit > 0
     assert limit <= 100
-    return await runtime_image_manager.search_runtime_images(page_id=page_id, limit=limit)
+    return await runtime_image_service.search_runtime_images(page_id=page_id, limit=limit)
 
 
 @router.get("/{id}")
 async def get_runtime_image(id: UUID) -> RuntimeImageInfo:
-    runtime_images = await runtime_image_manager.get_runtime_image(id)
+    runtime_images = await runtime_image_service.get_runtime_image(id)
     if runtime_images is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
     return runtime_images
@@ -33,5 +30,5 @@ async def get_runtime_image(id: UUID) -> RuntimeImageInfo:
 @router.get("/")
 async def batch_get_runtime_images(ids: list[UUID]) -> list[RuntimeImageInfo | None]:
     assert len(ids) <= 100
-    runtime_imagess = await runtime_image_manager.batch_get_runtime_images(ids)
+    runtime_imagess = await runtime_image_service.batch_get_runtime_images(ids)
     return runtime_imagess
